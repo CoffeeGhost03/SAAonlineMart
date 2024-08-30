e8a3467981ccd5dcd898cfddfce3c2c4f1db99bd
Heinrich Boshoff <hhoff03@outlook.com>
2024-08-30 18:42:26 +0200

    Add project files.

---
+++ SAAonlineMart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SAAonlineMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
